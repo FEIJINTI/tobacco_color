a4652427c71ab8cb139bfac79594e5455c01c259
FEIJINTI <83849113+FEIJINTI@users.noreply.github.com>
2022-07-18 16:33:55 +0800

    数据分析已经完成

---
+++ model.py
@@ -0,0 +1,79 @@
+# -*- codeing = utf-8 -*-
+# Time : 2022/7/18 14:03
+# @Auther : zhouchao
+# @File: model.py
+# @Software:PyCharm、
+import numpy as np
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+
+from elm import ELM
+
+
+class AnonymousColorDetector(object):
+    def __init__(self):
+        self.model = None
+
+    def fit(self, x: np.ndarray, world_boundary: np.ndarray, threshold: float, model_selected: str = 'elm',
+            negative_sample_size: int = 1000, train_size: float = 0.8, **kwargs):
+        """
+        拟合到指定的样本分布情况下，根据x进行分布的变化。
+
+        :param x: ndarray类型的正样本数据，给出的正样本形状为 n x feature_num
+        :param world_boundary: 整个世界的边界，边界形状为 feature_num个下限, feature_num个上限
+        :param threshold: 与正样本之间的距离阈值大于多少则不认为是指定的样本类别
+        :param model_selected: 选择模型，默认为elm
+        :param negative_sample_size: 负样本的数量
+        :param kwargs: 与模型相对应的参数
+        :return:
+        """
+        assert model_selected in ['elm']
+        if model_selected == 'elm':
+            node_num = kwargs.get('node_num', 10)
+            self.model = ELM(input_size=x.shape[1], node_num=node_num, output_num=2, **kwargs)
+        negative_samples = self.generate_negative_samples(x, world_boundary, threshold,
+                                                          sample_size=negative_sample_size)
+        data_x, data_y = np.concatenate([x, negative_samples], axis=0), \
+                         np.concatenate([np.ones(x.shape[0], dtype=int),
+                                         np.zeros(negative_samples.shape[0], dtype=int)], axis=0)
+        x_train, x_val, y_train, y_val = train_test_split(data_x, data_y, train_size=train_size, shuffle=True)
+
+        self.model.fit(x_train, y_train)
+        y_predict = self.model.predict(x_val)
+        print(classification_report(y_true=y_val, y_pred=y_predict))
+
+    def predict(self, x):
+        return self.model.predict(x)
+
+    @staticmethod
+    def generate_negative_samples(x: np.ndarray, world_boundary: np.ndarray, threshold: float, sample_size: int):
+        """
+        根据正样本和世界边界生成负样本
+
+        :param x: ndarray类型的正样本数据，给出的正样本形状为 n x feature_num
+        :param world_boundary: 整个世界的边界，边界形状为 feature_num个下限, feature_num个上限, array like
+        :param threshold: 与正样本x之间的距离限制
+        :return: 负样本形状为：(sample_size, feature_num)
+        """
+        feature_num = x.shape[1]
+        negative_samples = np.zeros((sample_size, feature_num), dtype=x.dtype)
+        generated_sample_num = 0
+        while generated_sample_num <= sample_size:
+            generated_data = np.random.uniform(world_boundary[:feature_num], world_boundary[feature_num:],
+                                               size=(sample_size, feature_num))
+            for sample_idx in range(generated_data.shape[0]):
+                sample = generated_data[sample_idx, :]
+                in_threshold = np.any(np.sum(np.power(sample - x, 2), axis=1) < threshold)
+                if not in_threshold:
+                    negative_samples[sample_idx, :] = sample
+                    generated_sample_num += 1
+                    if generated_sample_num >= sample_size:
+                        break
+        return negative_samples
+
+
+if __name__ == '__main__':
+    detector = AnonymousColorDetector()
+    x = np.array([[10, 30, 20], [10, 35, 25], [10, 35, 36]])
+    world_boundary = np.array([0, -127, -127, 100, 127, 127])
+    detector.fit(x, world_boundary, threshold=5, negative_sample_size=2000)
--- utils.py
+++ utils.py
@@ -26,13 +26,14 @@ class MergeDict(dict):
         return self
 
 
-def read_labeled_img(dataset_dir: str, color_dict: dict, ext='.bmp') -> dict:
+def read_labeled_img(dataset_dir: str, color_dict: dict, ext='.bmp', is_ps_color_space=True) -> dict:
     """
     根据dataset_dir下的文件创建数据集
 
     :param dataset_dir: 文件夹名称，文件夹内必须包含'label'和'label'两个文件夹，并分别存放同名的图像与标签
     :param color_dict: 进行标签图像的颜色查找
     :param ext: 图片后缀名,默认为.bmp
+    :param is_ps_color_space: 是否使用ps的标准lab色彩空间，默认True
     :return: 字典形式的数据集{label: vector(n x 3)},vector为lab色彩空间
     """
     img_names = [img_name for img_name in os.listdir(os.path.join(dataset_dir, 'label'))
@@ -45,25 +46,31 @@ def read_labeled_img(dataset_dir: str, color_dict: dict, ext='.bmp') -> dict:
         label_img = cv2.imread(label_path)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
         # 从opencv的色彩空间到Photoshop的色彩空间
-        alpha, beta = np.array([100 / 255, 1, 1]), np.array([0, -128, -128])
-        img = img * alpha + beta
-        img = np.asarray(np.round(img, 0), dtype=int)
+        if is_ps_color_space:
+            alpha, beta = np.array([100 / 255, 1, 1]), np.array([0, -128, -128])
+            img = img * alpha + beta
+            img = np.asarray(np.round(img, 0), dtype=int)
         dataset = {label: img[np.all(label_img == color, axis=2)] for color, label in color_dict.items()}
         total_dataset.merge(dataset)
     return total_dataset
 
 
-def lab_scatter(dataset: dict, class_max_num=None):
+def lab_scatter(dataset: dict, class_max_num=None, is_3d=False, is_ps_color_space=True):
     """
     在lab色彩空间内绘制3维数据分布情况
 
     :param dataset: 字典形式的数据集{label: vector(n x 3)},vector为lab色彩空间
     :param class_max_num: 每个类别最多画的样本数量，默认不限制
+    :param is_3d: 进行lab三维绘制或者a,b两通道绘制
+    :param is_ps_color_space: 是否使用ps的标准lab色彩空间，默认True
     :return: None
     """
     # 观察色彩分布情况
     fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
+    if is_3d:
+        ax = fig.add_subplot(projection='3d')
+    else:
+        ax = fig.add_subplot()
     for label, data in dataset.items():
         if class_max_num is not None:
             assert isinstance(class_max_num, int)
@@ -72,14 +79,25 @@ def lab_scatter(dataset: dict, class_max_num=None):
                 sample_idx = np.random.choice(sample_idx, class_max_num)
                 data = data[sample_idx, :]
         l, a, b = [data[:, i] for i in range(3)]
-        ax.scatter(a, b, l, label=label, alpha=0.1)
-    ax.set_xlim(-127, 127)
-    ax.set_ylim(-127, 127)
-    ax.set_zlim(0, 100)
+        if is_3d:
+            ax.scatter(a, b, l, label=label, alpha=0.1)
+        else:
+            ax.scatter(a, b, label=label, alpha=0.1)
+    x_max, x_min, y_max, y_min, z_max, z_min = [127, -127, 127, -127, 100, 0] if is_ps_color_space else \
+        [255, 0, 255, 0, 255, 0]
+    ax.set_xlim(x_min, x_max)
+    ax.set_ylim(y_min, y_max)
+    ax.set_xlabel('a*')
+    ax.set_ylabel('b*')
+    if is_3d:
+        ax.set_zlim(z_min, z_max)
+        ax.set_zlabel('L')
     plt.legend()
     plt.show()
 
 
 if __name__ == '__main__':
-    dataset = read_labeled_img("data/dataset", color_dict={(0, 0, 255): 1, (255, 0, 0): 2})
-    lab_scatter(dataset, class_max_num=2000)
+    color_dict = {(0, 0, 255): "yangeng", (255, 0, 0): "bejing", (0, 255, 0): "hongdianxian",
+                  (255, 0, 255): "chengsebangbangtang", (0, 255, 255): "lvdianxian"}
+    dataset = read_labeled_img("data/dataset", color_dict=color_dict, is_ps_color_space=False)
+    lab_scatter(dataset, class_max_num=20000, is_3d=False, is_ps_color_space=False)
