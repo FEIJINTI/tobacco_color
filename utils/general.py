6f2a9a1643358d3384c0f176a14652976e50b488
FEIJINTI <83849113+FEIJINTI@users.noreply.github.com>
2022-08-29 14:34:28 +0800

    修改了logging的bug

--- utils/general.py
+++ utils/general.py
@@ -78,7 +78,7 @@ def set_logging(name=None, verbose=VERBOSE):
         for h in logging.root.handlers:
             logging.root.removeHandler(h)  # remove all handlers associated with the root logger object
     rank = int(os.getenv('RANK', -1))  # rank in world for Multi-GPU trainings
-    logging.basicConfig(format="%(message)s", level=logging.INFO if (verbose and rank in (-1, 0)) else logging.WARNING)
+    # logging.basicConfig(format="%(message)s", level=logging.INFO if (verbose and rank in (-1, 0)) else logging.WARNING)
     return logging.getLogger(name)
 
 
